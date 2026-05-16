4c6d7c90886495d88eb00ff5d499d249f511dcba
Himanshu <ydvhimanshu461@gmail.com>
2026-05-16 10:51:42 +0530

    Upper bound

---
+++ Searching/UpperBound.java
@@ -0,0 +1,38 @@
+class UpperBound {
+
+    static int upperBound(int[] nums, int x) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        int ans = nums.length;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > x) {
+
+                ans = mid;
+
+                high = mid - 1;
+            }
+
+            else {
+
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,2,3};
+
+        int x = 2;
+
+        System.out.println("Upper Bound at : " + upperBound(nums, x));
+    }
+}
