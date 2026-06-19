f0b5d1dc34b6b3f95db74dcd7549f3afb6e5fadc
Himanshu <ydvhimanshu461@gmail.com>
2026-06-20 00:33:30 +0100

    Valid Anagram

---
+++ String/ValidAnagram.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String t = sc.nextLine();
+
+        System.out.println(isAnagram(s, t));
+
+        sc.close();
+    }
+}
