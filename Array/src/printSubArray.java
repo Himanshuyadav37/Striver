64ae356e99bb66717fb0423249479f17b9d65599
Himanshu <ydvhimanshu461@gmail.com>
2026-06-26 01:30:36 +0100

    print Sub Array

--- Array/src/printSubArray.java
+++ Array/src/printSubArray.java
@@ -1,3 +1,35 @@
 public class printSubArray {
-    
+
+    public static void pairSubArray(int[] arr) {
+
+        int totalSubArray = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int start = i;
+
+            for (int j = i; j < arr.length; j++) {
+
+                int end = j;
+
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+
+                totalSubArray++;
+                System.out.println();
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Total Subarrays = " + totalSubArray);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 6, 8, 10};
+
+        pairSubArray(arr);
+    }
 }
